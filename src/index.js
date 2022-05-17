38d0ece23b32168d1d996fbe7a4d1f23173d5090
abhishekchaurasiya <abhichek2580@gmail.com>
2022-05-17 13:07:56 +0530

    start new project

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
+const route = require("./routes/route");
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://mongoabhishek:JGETcKMFq8k1RFrV@cluster0.nn6fz.mongodb.net/project4/urlShortnerGroup27?retryWrites=true&w=majority", { useNewUrlParser: true })
+    .then(() => console.log('mongo is connected'))
+    .catch(error => console.log(error));
+
+app.use("/", route);
+
+app.listen(port, function () {
+    console.log(`Express app runnig on port ${port}`);
+})
+
---
+++ src/validator/validator.js
@@ -0,0 +1,10 @@
+let isValidData = function (value) {
+    if (typeof value === "undefined" || value === null) return false;
+    if (typeof value === "string" && value.trim().length === 0) return false;
+    return true;
+}
+
+let isValidRequestBody = function (requestBody) {
+    return Object.keys(requestBody).length > 0;
+};
+
